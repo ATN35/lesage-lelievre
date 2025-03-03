75620892b23b3719bc4d8560cc371215d7d19021
ATN <contact.atn35@gmail.com>
2025-03-03 18:48:31 +0100

    I have replaced all require() imports with import

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const authRoutes = require("./routes/authRoutes");
-const productRoutes = require("./routes/productRoutes");
-const reservationRoutes = require("./routes/reservationRoutes");
-const obituaryRoutes = require("./routes/obituaryRoutes");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import reservationRoutes from "./routes/reservationRoutes.js";
+import obituaryRoutes from "./routes/obituaryRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -25,4 +25,4 @@ app.use("/api/reservations", reservationRoutes);
 app.use("/api/obituaries", obituaryRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Serveur en cours sur le port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Serveur en cours sur le port ${PORT}`));
