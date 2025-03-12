9a55f251b54e348bfe0c58c93fe172d5280d8fe7
ATN <contact.atn35@gmail.com>
2025-03-12 16:48:46 +0100

    carousel image correction

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     {
       name: "La Guerche de Bretagne",
       images: [
-        "/images/chambre_funeraire_lesage-lelievre.jpg",
+        "/images/chambre_funeraire_lesage-lelievre_guerche",
         "/images/sarl-lesage-lelievre-img013bis-791w.jpg",
         "/images/sarl-lesage-lelievre-img062-1920w.jpg",
       ],
